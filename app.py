423fa9f622f7b969fd4655488f89122e74250f8f
Ankit Kumar <ankit.k@surveysparrow.com>
2021-01-06 21:53:59 +0530

    added generic Upload route

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+#Secret key if I decide to use sessions anytime.
 app.secret_key = "17BTRIS002"
 
+
+#Home route, to verify app status.
 @app.route('/', methods= ['GET'])
 def home():
-    return "I'm up."
+    return jsonify({
+        "Success" : "App is running."
+    })
+
+
+#Generic ETL route for testing.
+@app.route('/upload',methods=['POST', 'PUT'])
+def upload():
+    file = request.files['upload_file']
+    print(file.filename)
+    return jsonify({
+        "Success": "Read the file"
+        })
+
+
 
 
 if __name__ == "__main__":
